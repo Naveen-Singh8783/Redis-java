e3a0d7a0f20bee496c375abfbd392ee067a2b929
Naveen Singh <naveensingh4237@gmail.com>
2025-09-18 23:43:49 +0100

    removed userless comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
     try (ServerSocket serverSocket = new ServerSocket(port)) {
       // Since the tester restarts your program quite often, setting
       // SO_REUSEADDR ensures that we don't run into 'Address already in use'
-      // errors
+  
       serverSocket.setReuseAddress(true);
 
       while (listening) {
